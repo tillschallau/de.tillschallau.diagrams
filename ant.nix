3b0e03afb0793d25dd67c9ed42bdbac1362a6522
Till Schallau <till.schallau@tu-dortmund.de>
2022-09-05 16:26:43 +0200

    Remove nix files

--- ant.nix
+++
@@ -1,6 +0,0 @@
-import (builtins.fetchGit {
-    name = "apache-ant";
-    url = "https://github.com/nixos/nixpkgs-channels/";
-    ref = "refs/heads/nixpkgs-unstable";
-    rev = "c4364cdddc421dfdb4a60fda38ed955abce1603a";
-}) {}
